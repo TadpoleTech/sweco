136b06c594c9441bb4724cb9c0b3d92afda334cd
Erik Karsten <erik@opcode.xyz>
2024-03-23 01:31:51 +0200

    style: lint sql

--- tables.sql
+++ tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users(
+CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL,
@@ -9,26 +9,26 @@ CREATE TABLE users(
     is_admin BOOLEAN
 );
 
-CREATE TABLE boards(
+CREATE TABLE boards (
     board_id SERIAL PRIMARY KEY,
     header TEXT NOT NULL,
     pos_lat NUMERIC(7, 6),
     pos_lon NUMERIC(7, 6)
 );
 
-CREATE TABLE admin_privs(
+CREATE TABLE admin_privs (
     admin_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     board_id INT NOT NULL,
     FOREIGN KEY (user_id)
-        REFERENCES users (user_id)
-        ON DELETE CASCADE,
+    REFERENCES users (user_id)
+    ON DELETE CASCADE,
     FOREIGN KEY (board_id)
-        REFERENCES boards (board_id)
-        ON DELETE CASCADE
+    REFERENCES boards (board_id)
+    ON DELETE CASCADE
 );
 
-CREATE TABLE posts(
+CREATE TABLE posts (
     post_id SERIAL PRIMARY KEY,
     timestamp TIMESTAMP,
     owner_id INT NOT NULL,
@@ -38,36 +38,37 @@ CREATE TABLE posts(
     pos_lat NUMERIC(7, 6),
     pos_lon NUMERIC(7, 6),
     FOREIGN KEY (owner_id)
-        REFERENCES users (user_id)
-        ON DELETE CASCADE,
+    REFERENCES users (user_id)
+    ON DELETE CASCADE,
     FOREIGN KEY (board_id)
-        REFERENCES boards (board_id)
-        ON DELETE CASCADE
+    REFERENCES boards (board_id)
+    ON DELETE CASCADE
 );
 
-CREATE TABLE up_votes(
+CREATE TABLE up_votes (
     vote_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     post_id INT NOT NULL,
     FOREIGN KEY (user_id)
-        REFERENCES users (user_id)
-        ON DELETE CASCADE,
+    REFERENCES users (user_id)
+    ON DELETE CASCADE,
     FOREIGN KEY (post_id)
-        REFERENCES posts (post_id)
-        ON DELETE CASCADE
+    REFERENCES posts (post_id)
+    ON DELETE CASCADE
 
 );
 
-CREATE TABLE comments(
+CREATE TABLE comments (
     comment_id SERIAL PRIMARY KEY,
     post_id INT NOT NULL,
     timestamp TIMESTAMP,
     owner_id INT NOT NULL,
     content TEXT NOT NULL,
     FOREIGN KEY (owner_id)
-        REFERENCES users (user_id)
-        ON DELETE CASCADE,
+    REFERENCES users (user_id)
+    ON DELETE CASCADE,
     FOREIGN KEY (post_id)
-        REFERENCES posts (post_id)
-        ON DELETE CASCADE
+    REFERENCES posts (post_id)
+    ON DELETE CASCADE
 );
+
