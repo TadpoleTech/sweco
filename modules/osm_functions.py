d6fdaa8fc0ca78586c6dabab0e7745a472016e19
Erik Karsten <erik@opcode.xyz>
2024-03-23 12:45:30 +0200

    fix: stupid area search

--- modules/osm_functions.py
+++ modules/osm_functions.py
@@ -10,21 +10,21 @@ def city_filter(list_of_posts, cityname):
     '''Returns a dict of posts corrisponging to that city'''
     out = {}
     for post in list_of_posts:
-        post_city = query(post["pos_lon"], post["pos_lat"])
-        if post_city == cityname:
+        post_city = query(post["pos_lon"], post["pos_lat"])["addressparts"]["city"]
+        if post_city.lower() == cityname.lower():
             out[post["post_id"]] = post
+        else:
+            print(f"Post {post['post_id']} is not in {cityname}")
     return out
 
 
 def suburb_filter(list_of_posts, cityname, suburb_name):
     out = {}
     for post in list_of_posts:
-        post_address_data = query(post["pos_lon"], post["pos_lat"])[
-            "addressparts"]
-        if post_address_data['city'] == cityname and post_address_data['suburb'] == suburb_name:
+        post_address_data = query(post["pos_lon"], post["pos_lat"])["addressparts"]
+        if post_address_data['city'].lower() == cityname.lower() and post_address_data['suburb'].lower() == suburb_name.lower():
             out[post["post_id"]] = post
     return out
-
 
 if __name__ == "__main__":
     area = query(24.938379, 60.169855)
